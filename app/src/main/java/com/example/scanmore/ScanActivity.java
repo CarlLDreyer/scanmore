1ae7a1630f6966c6dca8aa56a239d0735e645ad6
CarlLDreyer <Carl.lidstrom.dreyer@gmail.com>
2019-02-19 17:17:59 +0100

    ASdasd

--- app/src/main/java/com/example/scanmore/ScanActivity.java
+++ app/src/main/java/com/example/scanmore/ScanActivity.java
@@ -1,6 +1,12 @@
 package com.example.scanmore;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class ScanActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
